c40286d1cdc45dbc3530accc9eb73dd320fda882
Ajaydeep Singh <asingh183@rrc.ca>
2026-04-22 09:29:28 -0500

    final lambda fixes

--- lambda/delete_inventory_item/lambda_function.py
+++ lambda/delete_inventory_item/lambda_function.py
@@ -1,12 +1,21 @@
+import json
 import boto3
 
-dynamodb = boto3.resource("dynamodb")
-table = dynamodb.Table("Inventory")
-
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('Inventory')
 
 def lambda_handler(event, context):
-    item_id = event["pathParameters"]["id"]
 
-    table.delete_item(Key={"item_id": item_id})
+    item_id = event.get("pathParameters", {}).get("id")
 
-    return {"statusCode": 200, "body": "Item deleted"}
+    if not item_id:
+        return {"statusCode": 400, "body": "Missing id"}
+
+    table.delete_item(
+        Key={"item_id": item_id}
+    )
+
+    return {
+        "statusCode": 200,
+        "body": "Item deleted"
+    }
